a2cccc4ef4cd4115781f100bfe38f4bcf6287840
gaeunnlee <gaeunnnleee@gmail.com>
2024-03-29 15:46:57 +0900

    feat: 공지사항 상세 조회 댓글

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -6,28 +6,22 @@ import Textarea from './ui/Textarea';
 import styled from 'styled-components';
 import Button from './ui/Button';
 
-export default function Comment({ postId }: { postId: number }) {
+export default function Comment({ postId, isScrollable }: { postId: number; isScrollable?: boolean }) {
    const { get, post } = useApi();
    const [commentList, setCommentList] = useState<ICommentResponse[]>();
    const [selectedComment, setSelectedComment] = useState<number>();
    const [textareaValue, setTextareaValue] = useState('');
-   const [textareaRef, setTextareaRef] =
-      useState<MutableRefObject<null | HTMLTextAreaElement>>();
+   const [textareaRef, setTextareaRef] = useState<MutableRefObject<null | HTMLTextAreaElement>>();
    const lastReplyRef = useRef<HTMLDivElement>(null);
    const commentListRef = useRef<HTMLDivElement>(null);
 
    const fetchCommentList = () => {
-      get({ api: `/post/mission-board/comments/${postId}`, auth: true }).then(
-         (response: ICommentResponse[]) => {
-            setCommentList(response);
-         }
-      );
+      get({ api: `/post/mission-board/comments/${postId}`, auth: true }).then((response: ICommentResponse[]) => {
+         setCommentList(response);
+      });
    };
 
-   const selectComment = (
-      e: React.MouseEvent<HTMLButtonElement>,
-      commentId: number
-   ) => {
+   const selectComment = (e: React.MouseEvent<HTMLButtonElement>, commentId: number) => {
       if (selectedComment === commentId) {
          setSelectedComment(0);
       } else {
@@ -36,9 +30,7 @@ export default function Comment({ postId }: { postId: number }) {
       }
    };
 
-   const postComment = async (
-      e: React.FormEvent<HTMLFormElement> | undefined
-   ) => {
+   const postComment = async (e: React.FormEvent<HTMLFormElement> | undefined) => {
       e !== undefined && e.preventDefault();
       await post({
          api: `${selectedComment ? `/post/mission-board/reply/${selectedComment}` : `/post/mission-board/comment/${postId}`}`,
@@ -54,7 +46,7 @@ export default function Comment({ postId }: { postId: number }) {
 
    useEffect(() => {
       fetchCommentList();
-   }, []);
+   }, [postId]);
 
    useEffect(() => {
       if (selectedComment !== 0 && selectedComment !== undefined) {
@@ -68,16 +60,11 @@ export default function Comment({ postId }: { postId: number }) {
    }, [commentList]);
 
    return (
-      <Container>
-         <CommentWrapper
-            ref={commentListRef}
-            selected={selectedComment !== undefined && selectedComment > 0}
-         >
+      <Container className="comment">
+         <CommentWrapper ref={commentListRef} selected={selectedComment !== undefined && selectedComment > 0} isScrollable={isScrollable}>
             {commentList?.map((item) => (
                <div key={item.id}>
-                  <div
-                     className={`flex flex-col justify-center p-3 gap-1 ${selectedComment === item.id && 'selected'}`}
-                  >
+                  <div className={`flex flex-col justify-center p-3 gap-1 ${selectedComment === item.id && 'selected'}`}>
                      <div className="flex gap-2 items-center">
                         <p>{item.author}</p>
                         <button
@@ -86,9 +73,7 @@ export default function Comment({ postId }: { postId: number }) {
                            }}
                            className=" text-zinc-400"
                         >
-                           {selectedComment === item.id
-                              ? '답글 취소'
-                              : '답글 달기'}
+                           {selectedComment === item.id ? '답글 취소' : '답글 달기'}
                         </button>
                      </div>
                      <div>{item.content}</div>
@@ -97,16 +82,13 @@ export default function Comment({ postId }: { postId: number }) {
                      {item.replies.map((reply) => (
                         <div key={reply.id}>
                            <span>{reply.author} &nbsp;</span>
-                           <span className="text-zinc-500">
-                              {reply.content}
-                           </span>
+                           <span className="text-zinc-500">{reply.content}</span>
                         </div>
                      ))}
                      <div ref={lastReplyRef}></div>
                   </div>
                </div>
             ))}
-            <div></div>
          </CommentWrapper>
          <form
             className={`flex flex-col gap-2 p-4 ${selectedComment ? 'h[7rem]' : 'h[5.5rem]'}`}
@@ -114,11 +96,7 @@ export default function Comment({ postId }: { postId: number }) {
                postComment(e);
             }}
          >
-            {selectedComment !== undefined && selectedComment > 0 && (
-               <span className="w-fit text-sm text-zinc-500 bg-[#edf6ff] px-2 py-1 rounded-full">
-                  답글 작성 중 ...
-               </span>
-            )}
+            {selectedComment !== undefined && selectedComment > 0 && <span className="w-fit text-sm text-zinc-500 bg-[#edf6ff] px-2 py-1 rounded-full">답글 작성 중 ...</span>}
             <div className="flex items-center p-4 bg-zinc-100 rounded-lg shadow-md">
                <Textarea
                   className="comment-textarea w-[calc(100%-50px)] bg-transparent "
@@ -129,11 +107,7 @@ export default function Comment({ postId }: { postId: number }) {
                   onKeyDown={(e) => {
                      e.key === 'Enter' && postComment(undefined);
                   }}
-                  placeholder={
-                     selectedComment !== 0
-                        ? '답글을 입력해주세요'
-                        : '댓글을 입력해주세요'
-                  }
+                  placeholder={selectedComment !== 0 ? '답글을 입력해주세요' : '댓글을 입력해주세요'}
                   setRef={setTextareaRef}
                />
                <label className="w-[50px] text-[2rem] cursor-pointer flex justify-center">
@@ -161,13 +135,15 @@ const Container = styled.div`
    }
 `;
 
-const CommentWrapper = styled.div<{ selected: boolean }>`
+const CommentWrapper = styled.div<{
+   selected: boolean;
+   isScrollable?: boolean;
+}>`
    box-sizing: border-box;
    display: flex;
    flex-direction: column;
    gap: 1rem;
-   height: ${(props) =>
-      props.selected ? 'calc(95dvh - 11rem)' : 'calc(95dvh - 9.5rem)'};
+   height: ${(props) => (props.isScrollable === false ? 'fit-content' : props.selected ? 'calc(95dvh - 11rem)' : 'calc(95dvh - 9.5rem)')};
    overflow: auto;
    overscroll-behavior: none;
 `;
--- src/pages/notice/detail.tsx
+++ src/pages/notice/detail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useApi } from '../../hooks/useApi';
 import { INotice } from '../../data/interface';
@@ -7,6 +7,7 @@ import { MdOutlineDateRange } from 'react-icons/md';
 import styled from 'styled-components';
 import { AiOutlineNotification } from 'react-icons/ai';
 import { FaRegComment } from 'react-icons/fa';
+import Comment from '../../components/Comment';
 
 export default function NoticeDetail() {
    const { pathname } = useLocation();
@@ -16,7 +17,7 @@ export default function NoticeDetail() {
    useEffect(() => {
       setNoticeId(pathname.split('/notice/')[1]);
    }, []);
-   useEffect(() => {
+   useLayoutEffect(() => {
       get({ api: `/notice/${noticeId}`, auth: true }).then((response) => {
          setContent(response);
       });
@@ -24,33 +25,28 @@ export default function NoticeDetail() {
 
    return (
       <Layout>
-         {content !== undefined && (
-            <Container>
-               <div className="flex flex-col gap-[0.3rem]">
-                  <div className="flex justify-between text-zinc-500 text-sm items-center">
-                     <p className=" bg-zinc-100 px-3 rounded-full ">
-                        {content.author}
-                     </p>
-                     <div className="flex gap-1 items-center text-sm">
-                        <MdOutlineDateRange className="text-zinc-500 text-[1.2rem]" />
-                        2024.03.26
-                     </div>
-                     <div className="flex gap-1 items-center text-sm">
-                        <FaRegComment className="text-zinc-500" />
-                        댓글 {content.commentCount}
-                     </div>
-                  </div>
-                  <p className="flex gap-1 items-center text-zinc-900 font-bold mt-2">
-                     {content.title}
-                  </p>
-
-                  <p className="text-zinc-700">{content.body}</p>
-               </div>
-            </Container>
-         )}
          <Container>
-            <p>댓글</p>
+            <div className="flex flex-col gap-[0.3rem]">
+               <div className="flex justify-between text-zinc-500 text-sm items-center">
+                  <p className=" bg-zinc-100 px-3 rounded-full ">{content?.author}</p>
+                  <div className="flex gap-1 items-center text-sm">
+                     <MdOutlineDateRange className="text-zinc-500 text-[1.2rem]" />
+                     2024.03.26
+                  </div>
+                  <div className="flex gap-1 items-center text-sm">
+                     <FaRegComment className="text-zinc-500" />
+                     댓글 {content?.commentCount}
+                  </div>
+               </div>
+               <p className="flex gap-1 items-center text-zinc-900 font-bold mt-2">{content?.title}</p>
+
+               <p className="text-zinc-700">{content?.body}</p>
+            </div>
          </Container>
+         <CommentContainer>
+            <p className="p-[1.2rem] pb-0">댓글</p>
+            <Comment postId={Number(noticeId)} isScrollable={false} />
+         </CommentContainer>
       </Layout>
    );
 }
@@ -62,6 +58,15 @@ const Container = styled.div`
    padding: 15px;
    gap: 0.8rem;
    border-radius: 10px;
-   box-shadow: 1px 1px 1px 0 #00000030;
-   cursor: pointer;
+   box-shadow: 1px 1px 1px 0 #00000020;
+`;
+
+const CommentContainer = styled.div`
+   background-color: #fdfdfd;
+   display: flex;
+   flex-direction: column;
+   border-radius: 10px;
+   box-shadow: 1px 1px 1px 0 #00000020;
+   .comment {
+   }
 `;
