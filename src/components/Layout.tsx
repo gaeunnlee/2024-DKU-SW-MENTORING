7adb6fff5c0dde3cf23f07121c5b253a09066293
gaeunnlee <gaeunnnleee@gmail.com>
2024-04-07 01:17:23 +0900

    feat: 페이지 전환 애니메이션 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 import styled from 'styled-components';
 import { useLayoutScrollStore } from '../stores/layout-scroll-stores';
 import { useNavStore } from '../stores/nav-stores';
+import { motion } from 'framer-motion';
 
 type LayoutProps = ComponentProps<'div'>;
 
@@ -26,18 +27,25 @@ export default function Layout({ children, className, ...props }: LayoutProps) {
             if (currentTarget.scrollTop < 100) {
                setIsNavVisible(true);
             } else {
-               setIsNavVisible(
-                  prevScroll > currentTarget.scrollTop &&
-                     currentTarget.scrollTop >= 0
-               );
-               setPrevScroll(
-                  currentTarget.scrollTop >= 0 ? currentTarget.scrollTop : 0
-               );
+               setIsNavVisible(prevScroll > currentTarget.scrollTop && currentTarget.scrollTop >= 0);
+               setPrevScroll(currentTarget.scrollTop >= 0 ? currentTarget.scrollTop : 0);
             }
          }}
          {...props}
       >
-         {children}
+         <motion.div
+            className="w-full flex flex-col gap-3"
+            variants={{
+               hidden: { y: 10, opacity: 0 },
+               enter: { opacity: 1, x: 0, y: 0 },
+            }}
+            initial="hidden"
+            exit={{ y: -10, opacity: 0 }}
+            animate="enter"
+            transition={{ type: 'linear' }}
+         >
+            {children}
+         </motion.div>
       </div>
    );
 }
