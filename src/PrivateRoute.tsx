dcc4db2052c4f46e0aeada60c6c04b8ac1a68d7a
gaeunnlee <gaeunnnleee@gmail.com>
2024-04-08 15:11:05 +0900

    chore: 로그인 페이지 이동시 afterLogout 값 수정

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -5,9 +5,5 @@ import { Navigate } from 'react-router-dom';
 export default function PrivateRoute({ children }: { children: ReactNode }) {
    const { isLoggedIn } = useAuth();
 
-   return isLoggedIn ? (
-      <>{children}</>
-   ) : (
-      <Navigate to={'/login'} replace={true} />
-   );
+   return isLoggedIn ? <>{children}</> : <Navigate to={'/login'} state={{ afterLogout: false }} replace={true} />;
 }
